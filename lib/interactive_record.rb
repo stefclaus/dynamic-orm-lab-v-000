e59a885314b4e8295f2ea60acb6b55fd0864cc32
Stef Claus <stef.claus@gmail.com>
2019-01-14 00:02:39 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -30,21 +30,38 @@ class InteractiveRecord
   end
 
   def table_name_for_insert
-  end
-
-  def col_names_for_insert
+    self.class.table_name
   end
 
   def values_for_insert
+    values = []
+      self.class.column_names.each do |col_name|
+        values << "'#{send(col_name)}'" unless send(col_name).nil?
+      end
+    values.join(", ")
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
   def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
   end
 
-  def self.find_by
+  def self.find_by(attribute)
+    column = attribute.to_a.join(', ').split(', ')
+          sql = <<-SQL
+              SELECT * FROM  #{table_name} WHERE #{column[0]} = '#{column[1]}'
+          SQL
+          DB[:conn].execute(sql)
   end
 
 
